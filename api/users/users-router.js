6b608bd732150e692395079cfb4d76e082644f1b
Mitch Hammond <mitch.b.hammond@gmail.com>
2024-04-04 14:19:14 -0600

    updates

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -35,11 +35,16 @@ router.post('/', validateUser,  (req, res, next) => {
   .catch(next)
 });
 
-router.put('/:id',validateUserId,validateUser, (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
-  console.log(req.user)
+router.put('/:id',validateUserId, validateUser, (req, res, next) => {
+ User.update(req.params.id, {name: req.name})
+  .then(()=>{
+    return User.getById(req.params.id)
+  })
+  .then(user =>{
+    res.json(user)
+  })
+
+  .catch(next)
 
 });
 
@@ -55,7 +60,7 @@ router.get('/:id/posts',validateUserId, (req, res) => {
   console.log(req.user)
 });
 
-router.post('/:id/posts',validateUserId, (req, res) => {
+router.post('/:id/posts',validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
